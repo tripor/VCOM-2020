898fcf242724ee41e28428faf572a2b0b27d163b
tripor <figorifico3000@gmail.com>
2020-03-03 17:12:25 +0000

    ex 7

---
+++ 7/7.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread("lines.jpg")    
+org = cv.imread("lines.jpg")
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+edges = cv.Canny(gray,50,150,apertureSize = 3)
+lines = cv.HoughLines(edges,1,np.pi/180,200)
+for line in lines:
+    rho,theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+    cv.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
+res = np.hstack((org,img))
+cv.imshow("houghlines",res)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+img = cv.imread("lines.jpg")    
+org = cv.imread("lines.jpg")
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+edges = cv.Canny(gray,50,150,apertureSize = 3)
+lines = cv.HoughLinesP(edges,1,np.pi/180,100,minLineLength=100,maxLineGap=10)
+for line in lines:
+    x1,y1,x2,y2 = line[0]
+    cv.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+    
+res = np.hstack((org,img))
+cv.imshow("houghlines",res)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+img = cv.imread('colours.png',0)
+img = cv.medianBlur(img,5)
+cimg = cv.cvtColor(img,cv.COLOR_GRAY2BGR)
+circles = cv.HoughCircles(img,cv.HOUGH_GRADIENT,1,20,
+                            param1=50,param2=30,minRadius=0,maxRadius=0)
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    cv.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    cv.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+cv.imshow('detected circles',cimg)
+cv.waitKey(0)
+cv.destroyAllWindows()
